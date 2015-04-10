c4f5358a527abefcdaa094a0829ef4a43c73a172
Arnukk <arnukk@gmail.com>
2015-04-11 00:04:06 +0400

    Added console plugin
    Added console plugin

--- afwan/platforms/android/assets/www/cordova_plugins.js
+++ afwan/platforms/android/assets/www/cordova_plugins.js
@@ -2,6 +2,9 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.apache.cordova.geolocation": "0.3.12",
+    "org.apache.cordova.console": "0.2.13"
+}
 // BOTTOM OF METADATA
 });
