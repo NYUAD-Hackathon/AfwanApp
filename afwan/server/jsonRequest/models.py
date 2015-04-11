0cd44f5ac7f7d1cb71ad7d489b111e93d2334e3c
cchen <cchen@masdar.ac.ae>
2015-04-11 09:02:10 +0400

    add basic models

---
+++ afwan/server/jsonRequest/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    minimumpayoff = models.FloatField()
+    def __str__(self):
+        return self.id
+
+class UserReq(models.Model):
+    userID = models.ForeignKey(User)
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    answered = models.BooleanField(default=False)
+    payoff = models.FloatField()
+
+
+class UserRes(models.Model):
+    userID = models.IntegerField()
+    content = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+    rank = models.IntegerField()
---
+++ afwan/server/server/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+# from jsonRequest.models import User
+
+urlpatterns = patterns('',
+                       # Examples:
+                       # url(r'^$', 'server.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^user/(?P<id>\d+)',
+                           'jsonRequest.views.getUser')
+                       )
