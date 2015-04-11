56e86a828c435b7f7bb694a51222575f59cf45c1
cchen <cchen@masdar.ac.ae>
2015-04-11 11:26:38 +0400

    dummy post request, get unsolved questions

--- afwan/server/jsonRequest/views.py
+++ afwan/server/jsonRequest/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from jsonRequest.models import User
+from jsonRequest.models import User, UserRequest, UserRespond
 from django.http import JsonResponse
+from django.http import HttpResponse
 from django.forms.models import model_to_dict
 # from django.core import serializers
 from django.core import serializers
-from django.utils import simplejson
+from django.views.decorators.csrf import csrf_protect
+import json
 
 # assuming obj is a model instance
 
@@ -15,3 +17,14 @@ def getUser(request, id):
     serialized_obj = serializers.serialize('json', [User.objects.get(id=1), ])
     # k =serializers.serialize("json",)
     return JsonResponse(serialized_obj, safe=False)
+
+
+def getUnsolved(request):
+    serialized_obj = serializers.serialize('json', UserRequest.objects.all())
+    return HttpResponse(serialized_obj)
+
+
+def postReq(request):
+    json_str = request.body.decode(encoding='UTF-8')
+    data = json.loads(json_str)
+    return HttpResponse(data['content'], status=200)
