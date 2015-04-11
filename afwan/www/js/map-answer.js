8392fb09e753800b8da91d1e56105226d32c579e
Kanbour Sama <samak@cmu.edu>
2015-04-12 00:43:10 +0300

    design fixes

--- afwan/www/js/map-answer.js
+++ afwan/www/js/map-answer.js
@@ -24,11 +24,11 @@ $(document).ready(function () {
         strokeWeight: 6
 	});
 });
-function CheckRes(x){
-          $.ajax({url:'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/res/check/',
-          type:"POST",
+// function CheckRes(x){
+//           $.ajax({url:'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/res/check/',
+//           type:"POST",
           
-          data:'{"respondID":4, "approved":'+x+'}',
-          success: function(){location.href="map.html";},
-          });
-}    
+//           data:'{"respondID":4, "approved":'+x+'}',
+//           success: function(){location.href="map.html";},
+//           });
+// }    
