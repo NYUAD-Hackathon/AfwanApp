d58a80a636dff5bcea2632c00b644490afa9c0fc
Arnukk <arnukk@gmail.com>
2015-04-11 17:29:02 +0400

    Added question feature (sending, receiving notification)
    Added question feature (sending, receiving notification)

--- afwan/www/js/index.js
+++ afwan/www/js/index.js
@@ -50,6 +50,9 @@ var app = {
 
 app.initialize();
 
+var Latitude = 0;
+var Longitude = 0;
+
 function onDeviceReady() {
 	// onSuccess Callback
 	// This method accepts a Position object, which contains the
@@ -57,6 +60,12 @@ function onDeviceReady() {
 	//
 	
 	var onSuccess = function(position) {
+		Latitude = position.coords.latitude;
+		Longitude = position.coords.longitude;
+		$("#submitQST").removeAttr('disabled');
+		$("#latitutefld").attr('value',Latitude);
+		$("#longitudefld").attr('value',Longitude);
+		/*
 		alert('Latitude: '          + position.coords.latitude          + '\n' +
 			  'Longitude: '         + position.coords.longitude         + '\n' +
 			  'Altitude: '          + position.coords.altitude          + '\n' +
@@ -65,6 +74,7 @@ function onDeviceReady() {
 			  'Heading: '           + position.coords.heading           + '\n' +
 			  'Speed: '             + position.coords.speed             + '\n' +
 			  'Timestamp: '         + position.timestamp                + '\n');
+		*/
 	};
 	
 
@@ -79,7 +89,63 @@ function onDeviceReady() {
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
+$( document ).ready(function() {
+		$("#submitQST").attr('disabled','disabled');
+		
+		$( "#questionmainform" ).submit(function( event ) {
+		    urlstr = "http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/req/";
+			var jsonData = '{"latitude":'+Latitude+', "longitude" :'+Longitude+', "payoff" : 1.5, "userID" :1 , "minimumRating" : 2, "content" : "' + $("#questiontxt").val() + '"}';
+			/*
+			var datatosend = [];
+			datatosend['latitude'] = Latitude;
+			datatosend['longitude'] = Longitude;
+			datatosend['payoff'] = 1.5;
+			datatosend['userid'] = 1;
+			datatosend['minimumRating'] = 2;
+			datatosend['content'] = $("#questiontxt").val();
+			*/
+			//var frm = $(this);
+			//var postdata = JSON.stringify(frm.serializeArray());
+			var postdata = $.parseJSON(jsonData);
+			var mypost = $.post(urlstr, jsonData).done( function(){
+				 BootstrapDialog.alert('Good job');
+			});
+			mypost.fail(function() {
+				BootstrapDialog.alert('I want banana!');
+			  });
+		    event.preventDefault();
+		});
+		
+		$("#slider").slider({
+				  animate: true,
+				  value:1,
+				  min: 0,
+				  max: 20,
+				  step: 1,
+				  slide: function(event, ui) {
+					  update(1,ui.value); //changed
+				  }
+		});
 
+		//Added, set initial value.
+		$("#amount").val(0);
+		$("#amount-label").text(0);
+		update();
+
+			  
+
+		//changed. now with parameter
+		function update(slider,val) {
+			//changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+			var $amount = slider == 1?val:0;
+
+			 $( "#amount" ).val($amount);
+			 $( "#amount-label" ).text($amount);
+			
+			 $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+		}
+
+});
 
 
 
