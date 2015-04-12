733560ccf61d49b9f2edd7b44d30ce579b60ecc5
cchen <cchen@masdar.ac.ae>
2015-04-12 09:26:30 +0400

    sent request, get returned ID, pass to next page

--- afwan/www/js/map-answer.js
+++ afwan/www/js/map-answer.js
@@ -1,4 +1,20 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    var queries = {};
+    $.each(document.location.search.substr(1).split('&'), function(c, q) {
+        var i = q.split('=');
+        queries[i[0].toString()] = i[1].toString();
+    });
+    $.ajax({
+        url: 'http://127.0.0.1:8000/req/ans/' + queries['reqid'],
+        type: "GET",
+        // data:'{"latitude": 30.0,"userID": 1,"longitude": 123.0,"payoff": 2.0,"minimumRating":2,"content": "'+question+'" }',
+        success: function(data) {
+            if (data.length == 0)
+                return;
+            else
+                console.log(data['list'][0].content)
+        },
+    });
     var map = new GMaps({
         div: '#map-canvas',
         lat: 24.522807,
@@ -12,20 +28,24 @@ $(document).ready(function () {
         panControl: false
     });
 
-	map.drawRoute({
+    map.drawRoute({
         origin: [24.522807, 54.435447],
         destination: [24.540274, 54.421541],
         travelMode: 'driving',
         strokeColor: '#131540',
         strokeOpacity: 0.6,
         strokeWeight: 6
-	});
+    });
 });
-function CheckRes(x){
-      $.ajax({url:'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/res/check/',
-      type:"POST",
-      
-      data:'{"respondID":4, "approved":'+x+'}',
-      	success: function(){location.href="map.html";},
-      });
+
+function CheckRes(x) {
+    $.ajax({
+        url: 'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/res/check/',
+        type: "POST",
+
+        data: '{"respondID":4, "approved":' + x + '}',
+        success: function() {
+            location.href = "map.html";
+        },
+    });
 }
