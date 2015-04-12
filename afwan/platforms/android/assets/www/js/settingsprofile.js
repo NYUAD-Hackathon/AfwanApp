ae033de43196565be1a66ff1495370bcf4b9398e
Arnukk <arnukk@gmail.com>
2015-04-12 12:54:06 +0400

    Finished the settings section. Added Questions feaching by id and profile tab
    Finished the settings section. Added Questions feaching by id and
    profile tab

--- afwan/platforms/android/assets/www/js/settingsprofile.js
+++ afwan/platforms/android/assets/www/js/settingsprofile.js
@@ -1,7 +1,20 @@
 
 $(document).ready(function(){
 	
-	if ($("#profiletabs li.active a").attr("href") == "#home"){
+	$( "#profiletabs li a" ).bind( "click", function() {
+		Smarttabs();
+	});
+	urlstr = "http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/user/1";
+			Slicedresp = {};
+			$.get(urlstr, function(data) {
+				$.each(data.result, function(idx, obj) {
+					$("#"+idx).val(obj);
+				});
+	});	
+});
+
+function Smarttabs(){
+	if ($("#profiletabs li.active a").attr("href") == "#profile"){
 			urlstr = "http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/user/1";
 			Slicedresp = {};
 			$.get(urlstr, function(data) {
@@ -9,7 +22,22 @@ $(document).ready(function(){
 					$("#"+idx).val(obj);
 				});
 			});
+	}else if($("#profiletabs li.active a").attr("href") == "#home"){
+			urlstr = "http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/user/req/1/";
+			Slicedresp = {};
+			$.get(urlstr, function(data) {
+				i = 0;
+                $.each(data.list, function(key, val) {
+					var temparray = {};
+					$.each(val, function(name, value){
+						if (name == "payoff" || name=="username" || name=="created_at" || name=="content"){
+							temparray[name] = value;
+						}
+					});
+					output = '<li><a href="#question'+i+'" data-transition="slide"> Asked on'+ temparray['created_at']+': </br>'+temparray['content']+'<span class="ui-li-count">'+temparray['payoff']+'</span></a></li>';
+					$('#listview').append(output).listview('refresh');
+					i++;
+                });
+			});
 	}
-
-	
-});
+}
