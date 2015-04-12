88bb88b4a2ffe5c5e1b4d4d360c842455d6296a4
Ken Leland III <kenleezle@gmail.com>
2015-04-12 12:12:09 +0400

    cordova build ios android

--- afwan/platforms/android/assets/www/js/map-question.js
+++ afwan/platforms/android/assets/www/js/map-question.js
@@ -2,88 +2,106 @@ var Latitude = 0;
 var Longitude = 0;
 
 function onSuccess(position) {
-    Latitude = position.coords.latitude;
-    Longitude = position.coords.longitude;
-    console.log('successfully retrieved position with lat:' + Latitude + 'and long' + Longitude);
-    $("#submitQST").removeAttr('disabled');
-    $("#latitutefld").attr('value',Latitude);
-    $("#longitudefld").attr('value',Longitude);
+        Latitude = position.coords.latitude;
+        Longitude = position.coords.longitude;
+        console.log('successfully retrieved position with lat:' + Latitude + 'and long' + Longitude);
+        $("#submitQST").removeAttr('disabled');
+        $("#latitutefld").attr('value', Latitude);
+        $("#longitudefld").attr('value', Longitude);
 
-    /*
-    var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: '           + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude;
-    */
-                        
-    var myLatlng = new google.maps.LatLng(Latitude,Longitude);
-    var mapOptions = {zoom: 4,center: myLatlng}
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        /*
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude;
+        */
 
-    var marker = new google.maps.Marker({position: myLatlng,map: map,title: 'Afwan'});
-}
-// onError Callback receives a PositionError object
-//
+        var myLatlng = new google.maps.LatLng(Latitude, Longitude);
+        var mapOptions = {
+            zoom: 4,
+            center: myLatlng
+        }
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Afwan'
+        });
+    }
+    // onError Callback receives a PositionError object
+    //
 function onError(error) {
-    onSuccess({coords: {latitude: 24.4667, longitude: 54.3667}});
+    onSuccess({
+        coords: {
+            latitude: 24.4667,
+            longitude: 54.3667
+        }
+    });
 }
 
 function initialize() {
     console.log('reached initialize function');
-    navigator.geolocation.getCurrentPosition(onSuccess, onError,{timeout:10000, maximumAge: 3000, enableHighAccuracy: true});
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+        timeout: 10000,
+        maximumAge: 3000,
+        enableHighAccuracy: true
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-$( document ).ready(function() {
-		$("#submitQST").attr('disabled','disabled');
-		
-		$( "#questionmainform" ).submit(function( event ) {
-            console.log('main form was submitted!');
-            
-            window.localStorage.setItem("question", $("#questiontxt").val());
+$(document).ready(function() {
+    $("#submitQST").attr('disabled', 'disabled');
 
-		    urlstr = "http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/req/";
-			var jsonData = '{"latitude":'+Latitude+', "longitude" :'+Longitude+', "payoff" : 1.5, "userID" :1 , "minimumRating" : 2, "content" : "' + $("#questiontxt").val() + '"}';
-			var postdata = $.parseJSON(jsonData);
-			var mypost = $.post(urlstr, jsonData).done( function(){
-                console.log('post was successful!');
-                $.notify('Your question has been submited','sucess');
-                location.href = "map-answer.html";
-			});
-			mypost.fail(function() {
-                 console.log('post failed!');
-                $.notify('We are encountering a problem, please retry!','fail');
+    $("#questionmainform").submit(function(event) {
+        console.log('main form was submitted!');
 
-			  });
-		    event.preventDefault();
-		});
-		
-		$("#slider").slider({
-				  animate: true,
-				  value:1,
-				  min: 0,
-				  max: 20,
-				  step: 1,
-				  slide: function(event, ui) {
-					  update(1,ui.value); //changed
-				  }
-		});
+        window.localStorage.setItem("question", $("#questiontxt").val());
 
-		//Added, set initial value.
-		$("#amount").val(0);
-		$("#amount-label").text(0);
-		update();
+        urlstr = "http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/req/";
+        var jsonData = '{"latitude":' + Latitude + ', "longitude" :' + Longitude + ', "payoff" : 1.5, "userID" :1 , "minimumRating" : 2, "content" : "' + $("#questiontxt").val() + '"}';
+        var postdata = $.parseJSON(jsonData);
+        var mypost = $.post(urlstr, jsonData).done(function(data) {
+            // console.log(data)
+            console.log('post was successful!');
+            $.notify('Your question has been submited', 'sucess');
+            location.href = "map-answer.html"
+            window.localStorage.setItem("questionID", data);
+        });
+        mypost.fail(function() {
+            console.log('post failed!');
+            $.notify('We are encountering a problem, please retry!', 'fail');
 
-			  
+        });
+        event.preventDefault();
+    });
 
-		//changed. now with parameter
-		function update(slider,val) {
-			//changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
-			var $amount = slider == 1?val:0;
+    $("#slider").slider({
+        animate: true,
+        value: 1,
+        min: 0,
+        max: 20,
+        step: 1,
+        slide: function(event, ui) {
+            update(1, ui.value); //changed
+        }
+    });
 
-			 $( "#amount" ).val($amount);
-			 $( "#amount-label" ).text($amount);
-			
-			 $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> '+$amount+' <span class="glyphicon glyphicon-chevron-right"></span></label>');
-		}
+    //Added, set initial value.
+    $("#amount").val(0);
+    $("#amount-label").text(0);
+    update();
+
+
+
+    //changed. now with parameter
+    function update(slider, val) {
+        //changed. Now, directly take value from ui.value. if not set (initial, will use current value.)
+        var $amount = slider == 1 ? val : 0;
+
+        $("#amount").val($amount);
+        $("#amount-label").text($amount);
+
+        $('#slider a').html('<label><span class="glyphicon glyphicon-chevron-left"></span> ' + $amount + ' <span class="glyphicon glyphicon-chevron-right"></span></label>');
+    }
 
 });
--- afwan/platforms/ios/www/js/map-answer.js
+++ afwan/platforms/ios/www/js/map-answer.js
@@ -1,31 +1,67 @@
-$(document).ready(function () {
-    var map = new GMaps({
-        div: '#map-canvas',
-        lat: 24.522807,
-        lng: 54.435447,
-        zoom: 12,
-        zoomControl: true,
-        zoomControlOpt: {
-            style: 'SMALL',
-            position: 'TOP_LEFT'
+var respondID;
+
+$(document).ready(function() {
+
+    var requsetID=window.localStorage.getItem("questionID")
+    $.ajax({
+        url: 'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/req/ans/' + requsetID,
+        type: "GET",
+        // data:'{"latitude": 30.0,"userID": 1,"longitude": 123.0,"payoff": 2.0,"minimumRating":2,"content": "'+question+'" }',
+        success: function(data) {
+            if (data.length == 0)
+                return;
+            else if (data['list'][0]) {
+                var ans = data['list'][0];
+                console.log(ans);
+                var template = $('#hidden-template').html();
+                // console.log($(template).find('#pro_name').html());
+
+                $('#answer-container').append(template);
+                $('#answer-container').find('#pro_name').html(ans['username'])
+                $('#answer-container').find('#answer-h4').html(ans['content'])
+                respondID= ans['id'];
+                $('.loader').hide();
+                $('.answer').show();
+                var map = new GMaps({
+                    div: '#map-canvas',
+                    lat: 24.522807,
+                    lng: 54.435447,
+                    zoom: 12,
+                    zoomControl: true,
+                    zoomControlOpt: {
+                        style: 'SMALL',
+                        position: 'TOP_LEFT'
+                    },
+                    panControl: false
+                });
+                if (ans['latitude'] & ans['longitude']) {
+                    map.drawRoute({
+                        origin: [24.522807, 54.435447],
+                        destination: [ans['longitude'], ans['latitude']],
+                        travelMode: 'driving',
+                        strokeColor: '#131540',
+                        strokeOpacity: 0.6,
+                        strokeWeight: 6
+                    });
+                }
+            }
         },
-        panControl: false
     });
 
-	map.drawRoute({
-        origin: [24.522807, 54.435447],
-        destination: [24.540274, 54.421541],
-        travelMode: 'driving',
-        strokeColor: '#131540',
-        strokeOpacity: 0.6,
-        strokeWeight: 6
-	});
 });
-// function CheckRes(x){
-//           $.ajax({url:'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/res/check/',
-//           type:"POST",
-          
-//           data:'{"respondID":4, "approved":'+x+'}',
-//           success: function(){location.href="map.html";},
-//           });
-// }    
+
+function CheckRes(x){
+      $.ajax({url:'http://ec2-54-149-21-125.us-west-2.compute.amazonaws.com/res/check/',
+      type:"POST",
+      data:'{"respondID":'+respondID+', "approved":'+x+'}',
+      success: function(){
+        if(x == 'false')
+          {
+          location.href="map-answer.html";}
+        else{
+            $('.btn-group').hide();
+            $('.meet').show();}
+        }
+      });
+
+}
